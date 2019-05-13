3f41d875326a7298dfd3bacd2753f9247f154774
Ranc58 <rancvova@gmail.com>
2019-05-13 21:08:16 +0300

    fix exceptions handling

--- app/server.py
+++ app/server.py
@@ -42,15 +42,15 @@ async def archive_handle(request):
             await response.write(chunk_archive)
             await asyncio.sleep(CONFIG['response_timeout'])
     except asyncio.CancelledError:
-        archive_process.kill()
         raise
     finally:
+        archive_process.kill()
         response.force_close()
     return response
 
 
 async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
+    async with aiofiles.open('app/index.html', mode='r') as index_file:
         index_contents = await index_file.read()
     return web.Response(text=index_contents, content_type='text/html')
 
